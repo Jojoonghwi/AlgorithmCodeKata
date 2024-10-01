46b033d2633b5c6a22c84f05055883afb91ebe22
JJH <whwndgnl12@naver.com>
2024-10-01 11:36:01 +0900

    바탕화면 정리

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-package 달리기경주;
+package 프로그래머스;
 
 public class Main {
     public static void main(String[] args) {
-        달리기경주.Solution solution = new 달리기경주.Solution();
+        Solution solution = new Solution();
 
         String[] String_arr_a = {"mumu", "soe", "poe", "kai", "mine"};
         String[] String_arr_b = {"kai", "kai", "mine", "mine"};
 
-        solution.solution(String_arr_a, String_arr_b);
+        solution.solution(String_arr_a);
     }
 }
