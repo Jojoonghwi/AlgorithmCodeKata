92ce881177c7770d4c95b6b6e29af5f453b09f5b
JJH <whwndgnl12@naver.com>
2024-10-01 12:13:18 +0900

    .

---
+++ 가운데글자가져오기/Solution.java
@@ -0,0 +1,17 @@
+package 가운데글자가져오기;
+//https://school.programmers.co.kr/learn/courses/30/lessons/12903
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int location=0;
+        if(s.length() % 2 == 0) {
+            location = s.length() / 2;
+            answer = s.substring(location-1, location + 1);
+        }
+        else {
+            location = s.length() / 2;
+            answer = s.substring(location, location + 1);
+        }
+        return answer;
+    }
+}
