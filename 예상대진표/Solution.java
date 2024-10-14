787d289c4b00ca5a986f6bdcc7f0cf3672aabe33
JJH <whwndgnl12@naver.com>
2024-10-14 10:47:33 +0900

    .

--- 예상대진표/Solution.java
+++ 예상대진표/Solution.java
@@ -13,6 +13,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             tonament.add(i + 1);
         }
+        //반으로 나눴을 떄 두 수가 다른 곳에 있는 경우
         while (tonament.size() > Math.pow(2.0, tonamentCount)) {
             if ((tonament.size() / 2 >= a && tonament.size() / 2 < b) || (tonament.size() / 2 < a && tonament.size() / 2 >= b)) {
                 while (count < tonament.size()) {
@@ -21,11 +22,13 @@ class Solution {
                 }
 
             }
+            //반으로 나눴을 떄 두 수가 작은 곳에 있는 경우
             else if (tonament.size() / 2 < a && tonament.size() / 2 < b) {
                 tonament.subList(0, tonament.size() / 2).clear();
                 a -= tonament.size();
                 b -= tonament.size();
             }
+            //반으로 나눴을 떄 두 수가 큰 곳에 있는 경우
             else if (tonament.size() / 2 >= a && tonament.size() / 2 >= b) {
                 tonament.subList(tonament.size() / 2, tonament.size()).clear();
             }
