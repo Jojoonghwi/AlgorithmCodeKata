7d09f9f9d51c5d918d5678354295ed855baec71a
JJH <whwndgnl12@naver.com>
2024-10-14 10:44:06 +0900

    .

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package 카펫;
+package 예상대진표;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +9,11 @@ public class Main {
         int[][] doubleIntARR = {{60, 50} ,{30, 70}, {60, 30}, {80, 40}};
         int[] intARR = {1,3,6,7};
         int[] intARR_2 = {1,3,6,7};
-        int k=18;
-        int n=6;
+        int i=1048576;
+        int j=12345;
+        int k=12346;
         String str="110010101001";
 
-        solution.solution(k,n);
+        solution.solution(i,j,k);
     }
 }
---
+++ 예상대진표/Solution.java
@@ -0,0 +1,36 @@
+package 예상대진표;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        int count = 1;
+        double tonamentCount = 0;
+        List<Integer> tonament = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            tonament.add(i + 1);
+        }
+        while (tonament.size() > Math.pow(2.0, tonamentCount)) {
+            if ((tonament.size() / 2 >= a && tonament.size() / 2 < b) || (tonament.size() / 2 < a && tonament.size() / 2 >= b)) {
+                while (count < tonament.size()) {
+                    count *= 2;
+                    tonamentCount++;
+                }
+
+            }
+            else if (tonament.size() / 2 < a && tonament.size() / 2 < b) {
+                tonament.subList(0, tonament.size() / 2).clear();
+                a -= tonament.size();
+                b -= tonament.size();
+            }
+            else if (tonament.size() / 2 >= a && tonament.size() / 2 >= b) {
+                tonament.subList(tonament.size() / 2, tonament.size()).clear();
+            }
+        }
+
+        return (int) tonamentCount;
+    }
+}
